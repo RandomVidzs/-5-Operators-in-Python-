0b2572dc66bdf1c0a2fc5f6c083da6cd88447749
Random Vidzs <calebpedicanjr@gmail.com>
2021-03-15 00:45:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#Operators
+# Addition       +
+# Subtraction    -
+# Multiplication *
+# Exponent       **
+# Division        /
+# Floor Division  //
+# Remainder       %
+
+#print ('--' * 16 'Addition' '--' * 16 )
+x = 5
+result = x + 10
+print (result)
+
+#print ('--' * 16 "Subtraction" '--' * 16 )
+x = 5
+result = x - 10
+print (result)
+
+#print ('--' * 16 "Division" '--' * 16 )
+x = 5
+result = x / 10
+print (result)
+
+#print ('--' * 16 "Exponent" '--' * 16 )
+x = 5
+result = x ** 2
+print (result)
+
+#print ('--' * 16 "Floor Division and Remainder" '--' * 16 )
+x = 5
+quotient = x // 2
+remainder = x % 2
+print ("quotient is", quotient)
+print ("Remainder is", remainder)
+
+print ("-" * 26)
+str1 = "hey "
+str2 = "jude "
+print(str1 + str2)
+
+print ("This are compound Operators")
+x,y = 5,6
+
+x = 5
+y = 6
+
+x = 5
+x += 10 # x = x + 10
+x -= 10 # x = x - 10
+
+print ("-" * 26)
+print ("Practice")
+
+#Convert distance to kilometers to miles
+#Create a Variable that will hold the two distances
+#kilometer = 1
+miles = 0.621371
+distance_km = 564.5
+
+print (miles + distance_km)
